4c96443300ff7b99ccb01ac0893d6d6cfeb9280b
CJjoelD <cjoelbenavides@gmail.com>
2025-12-18 20:23:31 -0500

    Fix index.js

--- index.js
+++ index.js
@@ -1,20 +1,12 @@
 const express = require("express");
 const app = express();
+
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.json({
-    mensaje: "Backend funcionando correctamente 🚀"
-  });
-});
-
-app.get("/salud", (req, res) => {
-  res.json({
-    estado: "OK",
-    servicio: "API de prueba"
-  });
+  res.json({ mensaje: "Backend desplegado correctamente 🚀" });
 });
 
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en puerto ${PORT}`);
+  console.log(`Servidor escuchando en puerto ${PORT}`);
 });
